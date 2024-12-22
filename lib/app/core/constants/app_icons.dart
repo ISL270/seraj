ca1bb25cd92126fe86eb78bf012dee476c01abca
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-12-22 12:11:42 +0200

    delete app icons file and use flutter gen runner package

--- lib/app/core/constants/app_icons.dart
+++
@@ -1,5 +0,0 @@
-abstract class AppIcons {
-  static const String hadith = 'assets/icons/quran.png';
-  static const String duas = 'assets/icons/duas.png';
-  static const String azkar = 'assets/icons/praying.png';
-}
