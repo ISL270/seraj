582395fdf5764bd782c5751306ddd999161a7585
Ahmed Sanad <ahmed.hac26@gmail.com>
2025-01-03 16:37:43 +0200

    implement cubit methods for toggle states

--- lib/app/features/add_other/presentation/cubit/add_other_cubit.dart
+++ lib/app/features/add_other/presentation/cubit/add_other_cubit.dart
@@ -1,6 +1,17 @@
 import 'package:athar/app/features/add_other/presentation/cubit/add_other_state.dart';
 import 'package:bloc/bloc.dart';
+import 'package:form_inputs/form_inputs.dart';
 
 class AddOtherCubit extends Cubit<AddOtherState> {
   AddOtherCubit() : super(const AddOtherState());
+
+  void otherTextChanged(String value) => emit(state.copyWith(other: Name.dirty(value)));
+
+  void sayerChanged(String value) => emit(state.copyWith(sayer: value));
+
+  void explanationChanged(String value) => emit(state.copyWith(description: value));
+
+  void sliderPriorityChanged(double value) => emit(state.copyWith(sliderValue: value));
+
+  Future<void> saveOtherForm() async {}
 }
