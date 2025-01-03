97600083e0685bfa59a5fc2f4bb635ee8f4534b9
Ahmed Sanad <ahmed.hac26@gmail.com>
2025-01-03 16:32:15 +0200

    create add other cubit

---
+++ lib/app/features/add_other/presentation/cubit/add_other_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:athar/app/features/add_other/presentation/cubit/add_other_state.dart';
+import 'package:bloc/bloc.dart';
+
+class AddOtherCubit extends Cubit<AddOtherState> {
+  AddOtherCubit() : super(const AddOtherState());
+}
---
+++ lib/app/features/add_other/presentation/cubit/add_other_state.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+final class AddOtherState extends Equatable {
+  const AddOtherState();
+
+  @override
+  List<Object> get props => [];
+}
