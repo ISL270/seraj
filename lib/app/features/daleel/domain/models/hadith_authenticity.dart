3764276357e6435cd2c784518f56a9664243d618
Ahmed Sanad <ahmed.hac26@gmail.com>
2025-01-05 20:28:58 +0200

    add hadith fields in daleel details based on daleel type

--- lib/app/features/daleel/domain/models/hadith_authenticity.dart
+++ lib/app/features/daleel/domain/models/hadith_authenticity.dart
@@ -1,7 +1,25 @@
+import 'package:athar/app/core/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+
 enum HadithAuthenticity { daif, hasan, sahih }
 
 extension HadithTypeX on HadithAuthenticity {
   bool get isDaif => this == HadithAuthenticity.daif;
   bool get isSahih => this == HadithAuthenticity.sahih;
   bool get isHasan => this == HadithAuthenticity.hasan;
+}
+
+extension HadithAuthenticityStringX on String {
+  String gethadithTypeString(BuildContext context) {
+    switch (this) {
+      case 'daif':
+        return context.l10n.hadithDaif;
+      case 'hasan':
+        return context.l10n.hadithHasan;
+      case 'sahih':
+        return context.l10n.hadithSahih;
+      default:
+        return '';
+    }
+  }
 }
--- lib/app/features/daleel_details/presentation/daleel_details_screen.dart
+++ lib/app/features/daleel_details/presentation/daleel_details_screen.dart
@@ -1,10 +1,12 @@
-// ignore_for_file: deprecated_member_use
+// ignore_for_file: deprecated_member_use, unused_local_variable
 
 import 'package:athar/app/core/extension_methods/bloc_x.dart';
 import 'package:athar/app/core/extension_methods/text_style_x.dart';
 import 'package:athar/app/core/l10n/l10n.dart';
 import 'package:athar/app/core/theming/app_colors_extension.dart';
 import 'package:athar/app/core/theming/text_theme_extension.dart';
+import 'package:athar/app/features/daleel/domain/models/daleel_type.dart';
+import 'package:athar/app/features/daleel/domain/models/hadith_authenticity.dart';
 import 'package:athar/app/features/settings/domain/settings.dart';
 import 'package:athar/app/features/settings/settings/settings_bloc.dart';
 import 'package:athar/app/widgets/screen.dart';
@@ -20,6 +22,7 @@ class DaleelDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const daleelType = DaleelType.hadith;
     return Screen(
       appBar: AppBar(
         leading: BackButton(
@@ -40,22 +43,25 @@ class DaleelDetailsScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 15.h,
             children: [
               Gap(10.h),
               _DaleelDetailsWidget(label: 'نوع الأثر', labelValue: context.l10n.propheticHadith),
+              Gap(15.h),
               const _DaleelDetailsWidget(
                 label: 'مضمون الأثر',
                 labelValue:
                     'من كانتِ الآخرةُ هَمَّهُ جعلَ اللَّهُ غناهُ في قلبِهِ وجمعَ لَه شملَهُ وأتتهُ الدُّنيا وَهيَ راغمة، ومن كانتِ الدُّنيا همَّهُ جعلَ اللَّهُ فقرَهُ بينَ عينيهِ وفرَّقَ عليهِ شملَهُ، ولم يأتِهِ منَ الدُّنيا إلَّا ما قُدِّرَ لَهُ',
               ),
+              Gap(15.h),
               const _DaleelDetailsWidget(label: 'قائل الأثر', labelValue: 'الرّسول ﷺ'),
+              Gap(15.h),
               Text(
                 'الكلمات المرجعية للأثر',
                 style: context.textThemeX.large.bold.copyWith(
                   color: context.colorsX.onBackgroundTint35,
                 ),
               ),
+              Gap(15.h),
               Wrap(
                 spacing: 3.w,
                 runSpacing: 10.h,
@@ -82,11 +88,36 @@ class DaleelDetailsScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              Gap(15.h),
               const _DaleelDetailsWidget(label: 'أهمية الأثر', labelValue: 'يجب حفظه'),
+              Gap(15.h),
               const _DaleelDetailsWidget(
                 label: 'شرح الأثر',
                 labelValue:
                     'الحديث الشريف يبين أن من يركز على الآخرة ويجعلها هدفه الرئيسي في الحياة، يمنحه الله القناعة والطمأنينة في قلبه، ويسهل له أموره، وتأتيه الدنيا بيسر وبركة دون عناء كبير. أما من يجعل الدنيا شغله الشاغل ويغفل عن الآخرة، فإنه يعيش في قلق دائم وشعور بالفقر والاحتياج، حتى لو امتلك الكثير، وتكون حياته مليئة بالمشكلات والتشتت، ولن يحصل من الدنيا إلا على ما قسمه الله له.',
+              ),
+              Gap(15.h),
+              Visibility(
+                child: Column(
+                  children: [
+                    _DaleelDetailsWidget(
+                      label: 'صحة الحديث',
+                      labelValue: HadithAuthenticity.hasan.name.gethadithTypeString(context),
+                    ),
+                    Gap(15.h),
+                  ],
+                ),
+              ),
+              Visibility(
+                child: Column(
+                  children: [
+                    _DaleelDetailsWidget(
+                      label: context.l10n.extractionOfHadith,
+                      labelValue: 'رواه ابن ماجه من حديث زيد بن ثابت بإسناد جيد ١٤٤١',
+                    ),
+                    Gap(15.h),
+                  ],
+                ),
               ),
               const _DaleelDetailsWidget(
                   label: 'وقت الإضافة الأثر', labelValue: '٠٣:٤٥ صباحًا\n١ يناير ٢٠٢٥'),
