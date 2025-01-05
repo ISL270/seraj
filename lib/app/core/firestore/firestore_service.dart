64781b7758cb90b41d563125cbad2ecef3520c72
Eslam Ashraf <SE.eslam.ashraf@gmail.com>
2025-01-05 09:27:30 +0200

    .

--- lib/app/core/firestore/firestore_service.dart
+++ lib/app/core/firestore/firestore_service.dart
@@ -6,8 +6,8 @@ import 'package:injectable/injectable.dart';
 @singleton
 final class FirestoreService {
   final users = _Users();
-  final hadith = _Hadith();
   final athar = _Athar();
+  final hadith = _Hadith();
   final others = _Others();
 }
 
@@ -18,20 +18,20 @@ final class _Users {
       collection.doc(userId).collection(daleelCollName);
 
   late final idField = 'id';
-  late final emailField = 'email';
   late final nameField = 'name';
+  late final emailField = 'email';
   late final daleelCollName = 'daleel';
 }
 
 sealed class _Daleel {
   late final id = 'id';
   late final text = 'text';
-  late final sayer = 'sayer';
-  late final description = 'description';
   late final tags = 'tags';
-  late final lastRevisedAt = 'lastRevisedAt';
+  late final sayer = 'sayer';
   late final priority = 'priority';
   late final daleelType = 'daleelType';
+  late final description = 'description';
+  late final lastRevisedAt = 'lastRevisedAt';
 }
 
 final class _Hadith extends _Daleel {
