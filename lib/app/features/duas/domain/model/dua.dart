813dd7e2fd2b53a140a48d9c885aa2b1cbe33452
Eslam Ashraf <SE.eslam.ashraf@gmail.com>
2025-01-05 10:33:21 +0200

    dua extends ismalic text

--- lib/app/features/duas/domain/model/dua.dart
+++ lib/app/features/duas/domain/model/dua.dart
@@ -1,27 +1,22 @@
-import 'package:athar/app/features/daleel/domain/models/priority.dart';
+import 'package:athar/app/core/models/domain/islamic_text.dart';
 import 'package:equatable/equatable.dart';
 
-final class Dua extends Equatable {
-  final String id;
-  final String dua;
+final class Dua extends IslamicText with EquatableMixin {
   final String? reward;
-  final Priority priority;
-  final List<String> tags;
-  final String? description;
 
   const Dua({
-    required this.id,
-    required this.dua,
-    required this.priority,
+    required super.id,
+    required super.text,
+    super.priority,
     this.reward,
-    this.description,
-    this.tags = const [],
+    super.description,
+    super.tags,
   });
 
   @override
   List<Object?> get props => [
         id,
-        dua,
+        text,
         tags,
         reward,
         priority,
