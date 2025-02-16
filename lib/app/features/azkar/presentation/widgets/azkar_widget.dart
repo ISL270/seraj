7df88f165f733f3ccdedd68a6c7f890826d23207
Ahmed Sanad <ahmed.hac26@gmail.com>
2025-02-16 20:14:47 +0200

    add delete method on azkar screen

--- lib/app/features/azkar/presentation/widgets/azkar_widget.dart
+++ lib/app/features/azkar/presentation/widgets/azkar_widget.dart
@@ -23,55 +23,92 @@ class _AzkarWidget extends StatelessWidget {
           ActionButton(
             icon: FontAwesomeIcons.trash,
             color: context.colorsX.error,
-            onTap: () => context.read<AzkarBloc>().add(AzkarDeleted(azkar.id!)),
+            onTap: () => _showDeleteDialog(context, context.read<AzkarBloc>()),
           ),
         ],
       ),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(12.w),
-        onTap: () => context.pushNamed(AzkarDetailsScreen.name, extra: azkar),
-        child: Container(
-          padding: EdgeInsets.all(4.sp),
-          margin: EdgeInsets.all(16.sp),
-          child: Padding(
-            padding: EdgeInsets.all(6.sp),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Gap(5.w),
-                    Expanded(
-                      child: Text(
-                        azkar.text,
-                        style: context.textThemeX.large
-                            .copyWith(fontFamily: GoogleFonts.amiriQuran().fontFamily),
-                      ),
-                    ),
-                    const Spacer(),
-                    IconButton(
-                      onPressed: () => context.read<AzkarBloc>().add(AzkarFavourited(azkar)),
-                      icon: AnimatedSwitcher(
-                        duration: const Duration(milliseconds: 400),
-                        transitionBuilder: (child, animation) {
-                          return ScaleTransition(scale: animation, child: child);
-                        },
-                        child: Icon(
-                          azkar.isFavourite ? Icons.favorite : Icons.favorite_border,
-                          key: ValueKey<bool>(azkar.isFavourite),
-                          color: azkar.isFavourite
-                              ? context.colorsX.error
-                              : context.colorsX.onBackground,
+      child: Padding(
+        padding: EdgeInsets.all(16.sp),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(12.w),
+          onTap: () => context.pushNamed(AzkarDetailsScreen.name, extra: azkar),
+          child: Container(
+            padding: EdgeInsets.all(4.sp),
+            child: Padding(
+              padding: EdgeInsets.all(6.sp),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Gap(5.w),
+                      Expanded(
+                        child: Text(
+                          azkar.text,
+                          style: context.textThemeX.large
+                              .copyWith(fontFamily: GoogleFonts.amiriQuran().fontFamily),
                         ),
                       ),
-                    ),
-                    Gap(15.w),
-                  ],
-                ),
-              ],
+                      IconButton(
+                        onPressed: () => context.read<AzkarBloc>().add(AzkarFavourited(azkar)),
+                        icon: AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 400),
+                          transitionBuilder: (child, animation) {
+                            return ScaleTransition(scale: animation, child: child);
+                          },
+                          child: Icon(
+                            azkar.isFavourite ? Icons.favorite : Icons.favorite_border,
+                            key: ValueKey<bool>(azkar.isFavourite),
+                            color: azkar.isFavourite
+                                ? context.colorsX.error
+                                : context.colorsX.onBackground,
+                          ),
+                        ),
+                      ),
+                      Gap(5.w),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  void _showDeleteDialog(BuildContext context, AzkarBloc azkarBloc) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        actionsAlignment: MainAxisAlignment.spaceAround,
+        backgroundColor: context.colorsX.background,
+        title: Center(
+          child: Text(
+            context.l10n.areYouSure,
+            style: context.textThemeX.large,
+          ),
+        ),
+        alignment: Alignment.center,
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: Text(
+              context.l10n.cancel.capitalized,
+              style: context.textThemeX.medium.copyWith(color: context.colorsX.error),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              context.pop();
+              azkarBloc.add(AzkarDeleted(azkar.id!));
+            },
+            child: Text(
+              context.l10n.confirm.capitalized,
+              style: context.textThemeX.medium.copyWith(color: context.colorsX.primary),
+            ),
+          ),
+        ],
       ),
     );
   }
